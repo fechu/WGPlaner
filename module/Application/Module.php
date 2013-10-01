92579788cc3f1df1963e65f80f091022995a570e
Sandro Meier <sandro.meier@fidelisfactory.ch>
2013-10-01 17:34:15 +0200

    Add the user repository as a service.

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -8,32 +8,55 @@
  */
 
 namespace Application;
-
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
-class Module
+class Module implements AutoloaderProviderInterface, ServiceProviderInterface
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
+	public function onBootstrap(MvcEvent $e)
+	{
+		$eventManager = $e->getApplication()->getEventManager();
+		$moduleRouteListener = new ModuleRouteListener();
+		$moduleRouteListener->attach($eventManager);
+	}
+	
+	public function getConfig()
+	{
+		return include __DIR__ . '/config/module.config.php';
+	}
+	
+	public function getAutoloaderConfig()
+	{
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+	
+	public function getServiceConfig()
+	{
+		return array(
+			'factories' => array(
+				
+				/**
+				 * The repository used by SMUser. In this application it is 
+				 * the UserRepository which implements the 
+				 * SMUser\Entity\Repository\UserRepositoryInterface
+				 */
+				'smuser.user_repository' => function($sm) {
+					/* @var $entityManager \Doctrine\ORM\EntityManager */
+					$entityManager = $sm->get('doctrine.entitymanager.orm_default');
+					$repo = $entityManager->getRepository('Application\Entity\User');
+					
+					return $repo;
+				}
+			),
+		);
+	}
+	
 }
