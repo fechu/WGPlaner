4f8abcd897389010782317a28a3ae0e9a4bd9922
Sandro Meier <sandro.meier@fidelisfactory.ch>
2013-10-27 21:06:55 +0100

    Implement the model for the count list.

---
+++ module/Application/src/Application/Entity/Repository/CountListRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file CountListRepository.php
+ * @date Oct 27, 2013 
+ * @author Sandro Meier
+ */
+ 
+namespace Application\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class CountListRepository extends EntityRepository0
+{
+	
+}
