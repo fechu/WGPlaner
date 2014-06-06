36e7dee416a1fc9b333797fc765e97a77cfb452e
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-06-06 10:44:09 +0200

    Change API

--- module/API/src/API/Controller/AbstractRestfulController.php
+++ module/API/src/API/Controller/AbstractRestfulController.php
@@ -1,59 +1,60 @@
 <?php
 /**
  * @file AbstractRestfulController.php
- * @date Nov 28, 2013 
+ * @date Nov 28, 2013
  * @author Sandro Meier
  */
- 
+
 namespace API\Controller;
 
 use SMCommon\Controller\AbstractRestfulController as SMCommonRestfulController;
+use Application\Entity\Account;
 
 class AbstractRestfulController extends SMCommonRestfulController
 {
 	/**
-	 * Get the identity provided. 
+	 * Get the identity provided.
 	 * Authentification is done via the API key in the X-API-KEY header.
-	 * @return A user object or void if authentication failed. 
+	 * @return A user object or void if authentication failed.
 	 */
 	protected function identity()
 	{
 		/* @var $request \Zend\Http\Request */
 		$request = $this->getRequest();
-		
+
 		// No one is logged in by default.
 		$user = null;
-		
+
 		if ($request->getHeaders()->has('X-API-KEY')) {
 			$apiKey = $request->getHeader('X-API-KEY')->getFieldValue();
 			if ($apiKey) {
 				// Check if this belong to a user.
 				/* @var $repo \Application\Entity\Repository\UserRepository */
 				$repo = $this->em->getRepository('Application\Entity\User');
-				
+
 				$user = $repo->findOneByApiKey($apiKey);
 			}
 		}
-		
+
 		return $user;
 	}
-	
+
 	/**
 	 * Takes the id from the route and fetches the purchase list.
-	 * @return PurchaseList
+	 * @return Account
 	 */
-	protected function getPurchaseList()
+	protected function getAccount()
 	{
-		$id = $this->getId('purchaselist');
-	
-		/* @var $repo \Application\Entity\Repository\PurchaseListRepository */
-		$repo = $this->em->getRepository('Application\Entity\PurchaseList');
-	
+		$id = $this->getId('account');
+
+		/* @var $repo \Application\Entity\Repository\AccountRepository */
+		$repo = $this->em->getRepository('Application\Entity\Account');
+
 		return $repo->find($id);
 	}
-	
+
 	/**
-	 * Creates a response, that indicates an invalid API Key. 
+	 * Creates a response, that indicates an invalid API Key.
 	 */
 	protected function invalidAPIKeyResponse()
 	{
--- module/API/src/API/Controller/PurchaseController.php
+++ module/API/src/API/Controller/PurchaseController.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @file PurchaseController.php
- * @date Nov 25, 2013 
+ * @date Nov 25, 2013
  * @author Sandro Meier
  */
- 
+
 namespace API\Controller;
 
 use Zend\View\Model\JsonModel;
@@ -13,54 +13,54 @@ use Application\Entity\Purchase;
 
 class PurchaseController extends AbstractRestfulController
 {
-	
+
 	public function __construct()
 	{
 		$this->identifierName = "purchaseid";
 	}
-	
+
 	/**
-	 * Create a new purchase. 
+	 * Create a new purchase.
 	 * @todo Deny adding of purchases to lists that do not belong to the user.
 	 */
 	public function create($data)
 	{
 		$user = $this->identity();
 		if ($user) {
-			$purchaseList = $this->getPurchaseList();
-			
+			$account = $this->getAccount();
+
 			// Form for validating the data.
 			$form = new PurchaseForm($this->em);
-			
+
 			$purchase = new Purchase();
 			$form->bind($purchase);
-			
+
 			/* @var $request \Zend\Http\Request */
 			$request = $this->getRequest();
 			$form->setData(array('PurchaseFieldset' => $data));
 			if ($form->isValid()) {
 				// Set the logged in user user who did the purchase.
 				$purchase->setUser($user);
-				$purchase->setPurchaseList($purchaseList);	// Add the purchase to this list.
+				$purchase->setAccount($account);	// Add the purchase to this list.
 				$this->em->persist($purchase);
 				$this->em->flush();
-		
+
 				// Success!
 				return $this->createdResponse();
 			}
 			else {
 				return $this->badRequestResponse($form->getMessages());
 			}
-			
+
 			return array(
 				'form' => $form,
-				'purchaseList' => $purchaseList,
+				'account' => $account,
 			);
 		}
 		else {
 			$this->invalidAPIKeyResponse();
 		}
 	}
-	
-	
+
+
 }
