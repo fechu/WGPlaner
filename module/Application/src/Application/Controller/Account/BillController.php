e24c68f7b459805d37101b07dc4f93143b06fbf0
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-06-26 20:59:18 +0200

    Add Bill form and create action

--- module/Application/src/Application/Controller/Account/BillController.php
+++ module/Application/src/Application/Controller/Account/BillController.php
@@ -9,6 +9,8 @@ namespace Application\Controller\Account;
 
 use Application\Entity\Account;
 use Application\Entity\Purchase;
+use Application\Form\BillForm;
+use Application\Entity\Bill;
 
 class BillController extends AbstractAccountController
 {
@@ -28,6 +30,37 @@ class BillController extends AbstractAccountController
 		return array(
 			'account' 	=> $account,
 			'bills'		=> $bills,
+		);
+	}
+
+	public function createAction()
+	{
+		// Create form and bind new bill to the form.
+		$form = new BillForm();
+		$bill = new Bill();
+		$form->bind($bill);
+
+
+		/* @var $request \Zend\Http\Request */
+		$request = $this->getRequest();
+
+		if ($request->isPost()) {
+			$form->setData($request->getPost());
+			if ($form->isValid()) {
+
+				// Persist the bill
+				$this->em->persist($bill);
+				$this->em->flush();
+
+				$parameters = array(
+					'accountid' => $this->getId('account'),
+				);
+				return $this->redirect()->toRoute('accounts/bills', $parameters);
+			}
+		}
+
+		return array(
+				'form' => $form,
 		);
 	}
 }
