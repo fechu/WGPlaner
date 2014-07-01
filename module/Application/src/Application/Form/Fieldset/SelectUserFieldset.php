79c989c711a4fd0715ba6abed0b80e72dab61458
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-07-01 22:44:24 +0200

    Move the Select user field to its own fieldset.

---
+++ module/Application/src/Application/Form/Fieldset/SelectUserFieldset.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @file SelectUserFieldset.php
+ * @date July 1, 2014 
+ * @author Sandro Meier
+ */
+ 
+namespace Application\Form\Fieldset;
+
+use SMCommon\Form\AbstractForm;
+use Doctrine\ORM\EntityManager;
+use Application\Entity\User;
+use Zend\Form\Fieldset;
+
+class SelectUserFieldset extends Fieldset 
+{
+	/**
+	 * The entity manager
+	 * @var EntityManager
+	 */
+	protected $em;
+	
+	/**
+	 * @param EntityManager $em 
+	 */
+	public function __construct(EntityManager $em)
+	{
+		parent::__construct('SelectUserFieldset');
+		
+		// Store the entity manager
+		$this->em = $em;
+		
+		// Add the select form 
+		$this->add(array(
+			'name' => 'user',
+			'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+			'options' => array(	
+				'object_manager'=> $em,
+				'target_class'	=> 'Application\Entity\User',
+				'property'	=> 'fullname',
+				'is_method'	=> true,
+				'label'		=> 'Benutzer',
+			),
+			'attributes' => array(
+				'required' => 'required'
+			)
+		));
+	}
+	
+	/**
+	 * @return User|Null The selected user. 
+	 */
+	public function getSelectedUser()
+	{
+		$userElement = $this->get('user');
+		$id = $userElement->getValue();
+		
+		return $this->em->find('Application\Entity\User', $id);
+	}
+}
--- module/Application/src/Application/Form/SelectUserForm.php
+++ module/Application/src/Application/Form/SelectUserForm.php
@@ -1,59 +1,45 @@
 <?php
 /**
  * @file SelectUserForm.php
- * @date Oct 15, 2013 
+ * @date Oct 15, 2013
  * @author Sandro Meier
  */
- 
+
 namespace Application\Form;
 
 use SMCommon\Form\AbstractForm;
 use Doctrine\ORM\EntityManager;
 use Application\Entity\User;
+use Application\Form\Fieldset\SelectUserFieldset;
 
 class SelectUserForm extends AbstractForm
 {
-	/**
-	 * The entity manager
-	 * @var EntityManager
-	 */
-	protected $em;
-	
-	/**
-	 * @param EntityManager $em 
-	 */
-	public function __construct(EntityManager $em)
-	{
-		parent::__construct('SelectUserForm');
-		
-		// Store the entity manager
-		$this->em = $em;
-		
-		// Add the select form 
-		$this->add(array(
-			'name' => 'user',
-			'type' => 'DoctrineModule\Form\Element\ObjectSelect',
-			'options' => array(	
-				'object_manager'=> $em,
-				'target_class'	=> 'Application\Entity\User',
-				'property'		=> 'fullname',
-				'is_method'		=> true,
-				'label'			=> 'Benutzer',
-			),
-			'attributes' => array(
-				'required' => 'required'
-			)
-		));
-	}
-	
-	/**
-	 * @return User|Null The selected user. 
-	 */
-	public function getSelectedUser()
-	{
-		$userElement = $this->get('user');
-		$id = $userElement->getValue();
-		
-		return $this->em->find('Application\Entity\User', $id);
-	}
+    /**
+     * The actual fieldset which contains the form elements
+     * @var SelectUserFieldset
+     */
+    protected $selectUserFieldset;
+
+    /**
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        parent::__construct('SelectUserForm');
+
+        // Store the entity manager
+        $this->em = $em;
+
+        // Add the fieldset
+        $this->selectUserFieldset = new SelectUserFieldset($em);
+        $this->add($this->selectUserFieldset);
+    }
+
+    /**
+     * @return User|Null The selected user.
+     */
+    public function getSelectedUser()
+    {
+        return $this->selectUserFieldset->getSelectedUser();
+    }
 }
