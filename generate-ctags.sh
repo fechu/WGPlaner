b1ef6ce8284c4d39e63843ec03c621d3113d5a65
Sandro Meier <sandro.meier@fidelisfactory.ch>
2014-07-01 11:44:52 +0200

    Refined ctags generation

--- generate-ctags.sh
+++ generate-ctags.sh
@@ -5,12 +5,17 @@ ctags -f vendor.tags \
     -R \
     --totals=yes \
     --fields=+aimS \
+    --languages=php \
+    --regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+    --regex-PHP='/interface ([^ ]*)/\1/c/' \
     vendor/
 
 # Generate ctags for the rest of the project.
 ctags -f tags \
     -R \
-    --verbose=yes \
     --totals=yes \
     --fields=+aimS \
+    --languages=php \
+    --regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+    --regex-PHP='/interface ([^ ]*)/\1/c/' \
     module/
