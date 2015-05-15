06c0fcda55bd9612ee082456e487df36c656ccc7
Sandro Meier <sandro.meier@fidelisfactory.ch>
2015-05-15 07:53:35 +0200

    Disable autocompletion in purchase form.

--- module/Application/src/Application/Form/PurchaseForm.php
+++ module/Application/src/Application/Form/PurchaseForm.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @file PurchaseForm.php
- * @date Oct 13, 2013 
+ * @date Oct 13, 2013
  * @author Sandro Meier
  */
- 
+
 namespace Application\Form;
 
 use SMCommon\Form\AbstractForm;
@@ -13,7 +13,7 @@ use Application\Form\Fieldset\PurchaseFieldset;
 class PurchaseForm extends AbstractForm
 {
 	protected $fieldset;
-	
+
 	/**
 	 * Create the form
 	 * @param The entity manager used to load autocomplete data.
@@ -21,13 +21,14 @@ class PurchaseForm extends AbstractForm
 	public function __construct($em)
 	{
 		parent::__construct();
-		
+
 		$fieldset = new PurchaseFieldset($em);
 		$fieldset->setUseAsBaseFieldset(true);
 		$this->fieldset = $fieldset;
 		$this->add($fieldset);
+		$this->setAttribute("autocomplete", "off");
 	}
-	
+
 	/**
 	 * Set the slip number
 	 * @param int $number
