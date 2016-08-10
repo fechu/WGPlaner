e71d58d23ffcd8916d40f1b837adcd3283204419
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-08-10 14:31:50 +0200

    Add switch to show all not verified purchases or only current users

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -8,17 +8,24 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-	// Check if we have unverified purchases.
-	$repo = $this->em->getRepository('Application\Entity\Purchase');
-	$purchases = $repo->findNotVerifiedPurchases();
-	if (count($purchases) == 0) {
-	    // Redirect to the accounts page.
-	    return $this->redirect()->toRoute('accounts/list-action');
-	}
+		// Check if we should show all not verified purchases or only the current users.
+        $showAll = (bool)($this->params()->fromQuery('all', 0));
+        $user = null;
+        if ($showAll == false) {
+            $user = $this->identity();
+        }
 
-	// We want to show the unverified purchases for verification.
-        return array(
-	    'purchases' => $purchases
-	);
-    }
+		// Check if we have unverified purchases.
+		$repo = $this->em->getRepository('Application\Entity\Purchase');
+		$purchases = $repo->findNotVerifiedPurchases($user);
+		if (count($purchases) == 0) {
+			// Redirect to the accounts page.
+			return $this->redirect()->toRoute('accounts/list-action');
+		}
+
+		// We want to show the unverified purchases for verification.
+		return array(
+			'purchases' => $purchases
+		);
+	}
 }
